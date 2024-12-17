f9964f5629cb63db3e57073cefeb26140734a364
Mattia Massarenti <mattia.mas2003@gmail.com>
2024-12-17 19:59:03 +0100

    update

--- src/main/java/org/jcam/effects/LiveEffect.java
+++ src/main/java/org/jcam/effects/LiveEffect.java
@@ -1,11 +1,13 @@
 package org.jcam.effects;
 
-import javafx.scene.image.ImageView;
-import lombok.NonNull;
 import org.jcam.lib.LiveEffectSingleton;
 
 public abstract class LiveEffect extends Effect implements LiveEffectSingleton {
     public LiveEffect() {
         super();
+    }
+
+    public static LiveEffect getInstance() {
+        return null;
     }
 }
